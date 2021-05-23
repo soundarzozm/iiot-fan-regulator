9e23c26993b445aa5a5e21a8c67c0016e347b017
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-05-23 21:09:31 +0530

    rectified math

---
+++ web/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import joblib
+
+app = Flask(__name__)
+api = Api(app)
+
+model = joblib.load("model.pkl")    
+
+class Predict(Resource):
+    def post(self):
+
+        postedData = request.get_json()
+        temperature = postedData["temperature"]
+
+        y_pred = model.predict([[temperature]])
+
+        if y_pred > 10:
+            fanSpeed = 10
+        elif y_pred < 0:
+            fanSpeed = 0
+        else:
+            fanSpeed = y_pred - (y_pred - int(y_pred))        
+
+        retJson = {
+        "status": 200,
+        "msg": "success",
+        "fanSpeed" : fanSpeed
+        }
+
+        return jsonify(retJson)
+
+api.add_resource(Predict, "/predict")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
